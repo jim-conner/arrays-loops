8ae572a8625eed199e423e88d0bb6dff45d1debe
jimconner <jamesdavidconner@gmail.com>
2021-01-19 20:38:29 -0600

    did some array methods practice

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("connected")
+const array1 = ['cow', 'dog', 'cat'];
+const string1 = array1.join(' ')
+const string1ToArray =string1.split(' ')
+// console.log(string1)
+// console.log(string1ToArray);
+
+// ************* .push() and .pop() *************
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1); //notice on 2nd console.log cat is back
+
+// ************* .unshift() and .shift() *************
+array1.unshift('cow');
+console.log(array1);
+array1.shift('cat');
+console.log(array1);
